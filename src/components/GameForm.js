f3e63a73b7c75b608826ff972964443181359b41
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-01-10 23:47:53 -0500

    creating game form

---
+++ src/components/GameForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class GameForm extends Component {
+
+    state={
+        title:"",
+        release_year:"",
+        console:"",
+        developer:"",
+        genre:"",
+        description:"",
+        cover_art:""
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    render() {
+        return (
+            <div className="gameForm" onSubmit={this.handleSubmit}>
+                <form>
+                    <label>Title</label>
+                        <input type='text' name='title' value={this.state.title} onChange={(e) => this.handleChange(e)}/>
+                    <label>Release Year</label>
+                        <input type='text' name='release_year' value={this.state.release_year} onChange={(e) => this.handleChange(e)}/>
+                    <label>Console</label>
+                        <input type='text' name='console' value={this.state.console} onChange={(e) => this.handleChange(e)}/>
+                    <label>Developer</label>
+                        <input type='text' name='developer' value={this.state.developer} onChange={(e) => this.handleChange(e)}/>
+                    <label>Genre</label>
+                        <input type='text' name='genre' value={this.state.genre} onChange={(e) => this.handleChange(e)}/>
+                    <label>Description</label>
+                        <input type='text' name='description' value={this.state.description} onChange={(e) => this.handleChange(e)}/>
+                    <label>Cover Art URL</label>
+                        <input type='text' name='cover_art' value={this.state.cover_art} onChange={(e) => this.handleChange(e)}/>
+                    <input type='submit' />
+                </form>
+                
+            </div>
+        );
+    }
+}
+
+export default GameForm;
