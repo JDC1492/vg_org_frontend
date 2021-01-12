2a4b7fd1a7a38741eb26813e060306b00be3ceb3
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-01-12 11:55:16 -0500

    removed hard coded state

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,12 +1,5 @@
 export default function gamesReducer(
-    state=[{id:15, title:"Legend of Dragoon",
-        release_year:"2001",
-        console:"Playstation",
-        developer:"Japan Studio", 
-        genre:"JRPG", 
-        description:"Dragons and Knights",
-        cover_art:"https://upload.wikimedia.org/wikipedia/en/3/32/Legend_of_Dragoon.jpg"
-        }], action){
+    state=[], action){
         switch (action.type){
         case 'SET_GAMES':
             return[...action.payload]
