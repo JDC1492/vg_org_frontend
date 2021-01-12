4adf54d72342290fe119fb64b3208e9d83150baf
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-01-12 11:49:58 -0500

    fetch action written and reducer updated

--- src/actions/actIndex.js
+++ src/actions/actIndex.js
@@ -1,3 +1,13 @@
+export const fetchGames = () => {
+    return (dispatch) => {
+        return fetch('http://localhost:3000/games')
+            .then(resp => resp.json())
+            .then(games => {
+                dispatch({type: "SET_GAMES", payload: games})
+            })
+    }
+}
+
 export const addGame = game => {
     return {
         type:'ADD_GAME', payload: game
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -8,11 +8,13 @@ export default function gamesReducer(
         cover_art:"https://upload.wikimedia.org/wikipedia/en/3/32/Legend_of_Dragoon.jpg"
         }], action){
         switch (action.type){
+        case 'SET_GAMES':
+            return[...action.payload]
         case 'ADD_GAME':
             return[...state, action.payload]  
         case 'REMOVE_GAME':
             let newGames = state.filter(game => game.id !== action.payload)
-            return[...newGames]   
+            return[...newGames]  
         default:
             return state
         }
