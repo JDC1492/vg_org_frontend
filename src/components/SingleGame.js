39bba81168403d9a387453579ebb3b2a6b3b476e
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-01-13 20:55:29 -0500

    show page edit done/workin on show route issue

--- src/components/SingleGame.js
+++ src/components/SingleGame.js
@@ -2,6 +2,8 @@ import React from 'react';
 // import { connect } from 'react-redux';
 import { Card, Container, ListGroup, Row } from 'react-bootstrap';
 
+
+
 const SingleGame = (props) => {
         const urlID = parseInt(window.location.href.charAt(window.location.href.length-1))
          const game = props.games.find(game => game.id === urlID)
@@ -9,15 +11,19 @@ const SingleGame = (props) => {
         return (
             <div>
                 <Row>
-                    <Container>
-                        <Card className="card-look" style={{ width: '20rem'}}>      
-                            <Card.Img  horizontal variant="top" src={game.cover_art} alt="" />
-                            <Card.Body>
-                                <Card.Title>{game.title}</Card.Title> 
-                                <Card.Text className="justify-content-md-center" horizontal>{game.description}</Card.Text>
-                            </Card.Body>    
+                    <div className='show-card'>
+                        <Card>      
+                            <Card.Img  max-width="auto" height="auto" style={{imageRendering:'pixelated'}}variant="bottom" src={game.cover_art} alt="" />
+                                <Card.Body>
+                                    <Card.Title>{game.title}</Card.Title> 
+                                    <ListGroup.Item>Release Year: {game.release_year}</ListGroup.Item>
+                                    <ListGroup.Item>Console: {game.console}</ListGroup.Item>
+                                    <ListGroup.Item>Developer: {game.developer}</ListGroup.Item>
+                                    <ListGroup.Item>Game Genre: {game.genre}</ListGroup.Item> 
+                                    <ListGroup.Item>{game.description}</ListGroup.Item>
+                                </Card.Body>                   
                         </Card>
-                    </Container>
+                    </div>
                 </Row>
             </div>
         );
@@ -25,5 +31,3 @@ const SingleGame = (props) => {
 
 
 export default SingleGame;
-
-                             
