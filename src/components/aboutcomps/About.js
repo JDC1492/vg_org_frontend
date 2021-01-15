5bf3b1383c5d807e6fce2c64035ea991a007b2f4
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-01-15 17:52:57 -0500

    about page edits

--- src/components/aboutcomps/About.js
+++ src/components/aboutcomps/About.js
@@ -9,16 +9,22 @@ class About extends Component {
             <br/>
             <p>
                 <Container>
-                My name is Jonathan Cohen. I reccently completed the curriculum of the Flatiron School and am preparing for 
+                My name is Jonathan. I reccently completed the curriculum of the Flatiron School and am preparing for 
                 my final project review. My goal has always been to somehow fuse the love I have for music and video games with 
                 this new found interest and enjoyment of coding. This final project is a start of something that I plan to grow
-                after graduation.  
+                after graduation.   
                 <br/><br/>
                 VG_organizer has been a project idea that I've used for both my Ruby on Rails project and My React Project. 
                 While this project currently showcases basic react knowledge, the plan is to continue to allow this platform
                 to grow with features as I become a better and more knowledgeable developer. My plan for this is to watch the 
                 progress that the site has gone through as I've grow as a developer. While this will be used for personal use
-                for myself I would like to imagine that at one point I can release this on heroku and get real user feedback.
+                for myself I would like to imagine that at one point I can release this on heroku and get real user feedback.  
+                Currently the list of games mean something personally to me. All have given me memories that I'll keep forever,
+                some through the story-telling and others through the beautiful music that aided in the creation of teh wolrds
+                that I had a chance to experience. 
+                <br/><br/>
+                I appreciate my time at the Flatiron School. I've met some great individuals in my time here and have learned so much. 
+                Im excited to go forward and grow as a developer.
                 </Container>
 
             </p>
